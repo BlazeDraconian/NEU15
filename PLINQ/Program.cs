459e0d8ab93a52b591bf8cd13413a5adabeae1fc
Blaze_Draconian <Dangus360@gmail.com>
2025-10-20 13:51:50 +0200

    Added new classes

---
+++ PLINQ/Program.cs
@@ -0,0 +1,22 @@
+﻿//var list = Enumerable.Range(1, 20)
+//    .AsParallel()
+//    .Select(i => CostlyOperation(i))
+//    .ToList();
+//Console.WriteLine($"Processing complete. Here is the result:\n ");
+//Console.WriteLine(string.Join(",", list));
+
+
+//static int CostlyOperation(int n)
+//{
+//    Thread.Sleep(500);
+//    Console.WriteLine($"Processing object {n}");
+//    return n;
+//}
+
+long sum = Enumerable.Range(1, 1_000_000_000)
+    .AsParallel()
+    .Where(i => i % 3 == 0 || i % 5 == 0)
+    .Select(i => (long)i)
+    .Sum();
+Console.WriteLine(sum);
+Console.WriteLine();
---
+++ Tasks/Program.cs
@@ -0,0 +1,23 @@
+﻿
+//Task myTask = Task.Run(TaskOne);
+Task myTask = new Task(TaskOne);
+
+Console.WriteLine($"MyTask status is : {myTask.Status}"); 
+
+myTask.Start();
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine($"Main process:{i}");
+}
+
+Console.WriteLine($"MyTask status is : {myTask.Status}");
+Console.WriteLine("The end");
+static void TaskOne()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine($"Task 1: {i}");
+    }
+    Console.WriteLine("Running task 1");
+}
