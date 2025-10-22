7347451a80b16c743254b47c5d0f71d307806fb7
Blaze_Draconian <Dangus360@gmail.com>
2025-10-22 13:33:03 +0200

    Updated Async

--- Async_&_Await/Program.cs
+++ Async_&_Await/Program.cs
@@ -4,17 +4,31 @@
     private static async Task Main(string[] args)
     {
         Console.WriteLine("Program start");
-        await Download();
+        var myTask = Download();
+        int dataLength = await myTask;
+        Console.WriteLine($"Got {dataLength} chars of data");
         Console.WriteLine("Program end");
     }
 
-    private static async Task Download()
+    private static async Task<int> Download()
     {
         Task<string> download = Client.GetStringAsync("http://google.com");
         Console.WriteLine("Download started...");
         string data = await download;
         Console.WriteLine("Download complete");
 
-        Console.WriteLine($"Got {data.Length} chars of data");
+        //Console.WriteLine($"Got {data.Length} chars of data");
+        await Task.Delay(1000);
+        Console.WriteLine("Completed unnecessary delay in download");
+        Console.WriteLine("Start CPU-bound operation asynchronosly");
+
+        await Task.Run(() =>
+        {
+            for (int i = 0; i < 1_000_000_000; i++)
+            {
+
+            }
+        });
+        return data.Length;
     }
 }
