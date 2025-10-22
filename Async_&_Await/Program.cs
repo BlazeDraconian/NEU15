5ae81c8868d03162ca0645231d1e5d4f5f601234
Blaze_Draconian <Dangus360@gmail.com>
2025-10-22 11:54:47 +0200

    Added Async and Await

---
+++ Async_&_Await/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    private static HttpClient Client = new HttpClient();
+    private static async Task Main(string[] args)
+    {
+        Console.WriteLine("Program start");
+        await Download();
+        Console.WriteLine("Program end");
+    }
+
+    private static async Task Download()
+    {
+        Task<string> download = Client.GetStringAsync("http://google.com");
+        Console.WriteLine("Download started...");
+        string data = await download;
+        Console.WriteLine("Download complete");
+
+        Console.WriteLine($"Got {data.Length} chars of data");
+    }
+}
