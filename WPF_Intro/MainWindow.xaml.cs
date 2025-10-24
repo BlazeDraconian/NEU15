228518bce39404144d7fecf9461e0bd2a75033d4
Blaze_Draconian <Dangus360@gmail.com>
2025-10-24 12:09:17 +0200

    Added new classes

---
+++ WPF_Intro/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPF_Intro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MyButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                button.Content += "!";
+            }
+            
+            //MessageBox.Show("Hello World");
+
+            //LeftButton.Content = "Hello";
+            //MyButton.HorizontalAlignment = HorizontalAlignment.Left;
+        }
+    }
+}
