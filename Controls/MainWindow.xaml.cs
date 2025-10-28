717a70fda69ff7e4f3c3b9f195813bee4fbc9092
Blaze_Draconian <Dangus360@gmail.com>
2025-10-28 13:31:02 +0100

    Added exercises

---
+++ Controls/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            textBox.Text = "Hello World";
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void textBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                label.Content = e.Key;
+            }
+
+
+        }
+    }
+}
